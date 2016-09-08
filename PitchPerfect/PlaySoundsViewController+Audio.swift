808bb43bb884b255a6eae2cf1423f8233cc94f33
Work <work@Pauls-MacBook-Pro.local>
2016-09-07 22:26:49 -0400

    Pitch Perfect 1.0

---
+++ PitchPerfect/PlaySoundsViewController+Audio.swift
@@ -0,0 +1,194 @@
+//
+//  PlaySoundsViewController+Audio.swift
+//  PitchPerfect
+//
+//  Copyright © 2016 Udacity. All rights reserved.
+//
+import UIKit
+import AVFoundation
+
+extension PlaySoundsViewController: AVAudioPlayerDelegate {
+    struct Alerts {
+        static let DismissAlert = "Dismiss"
+        static let RecordingDisabledTitle = "Recording Disabled"
+        static let RecordingDisabledMessage = "You've disabled this app from recording your microphone. Check Settings."
+        static let RecordingFailedTitle = "Recording Failed"
+        static let RecordingFailedMessage = "Something went wrong with your recording."
+        static let AudioRecorderError = "Audio Recorder Error"
+        static let AudioSessionError = "Audio Session Error"
+        static let AudioRecordingError = "Audio Recording Error"
+        static let AudioFileError = "Audio File Error"
+        static let AudioEngineError = "Audio Engine Error"
+    }
+    
+    // raw values correspond to sender tags
+    enum PlayingState { case Playing, NotPlaying }
+
+    
+    // MARK: Audio Functions
+    
+    func setupAudio() {
+        print("\(self.dynamicType).setupAudio()")
+        // initialize (recording) audio file
+        do {
+            audioFile = try AVAudioFile(forReading: recordedAudioURL)
+        } catch {
+            showAlert(Alerts.AudioFileError, message: String(error))
+        }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, withOptions: .DefaultToSpeaker)
+        } catch {
+            print("Could not set AVAudionSession category to .DefaultToSpeaker")
+        }
+    }
+    
+    func playSound(rate rate: Float? = nil, pitch: Float? = nil, echo: Bool = false, reverb: Bool = false) {
+        print("\(self.dynamicType).playSound(\(rate),\(pitch),\(echo),\(reverb))")
+        
+        // initialize audio engine components
+        audioEngine = AVAudioEngine()
+
+        // node for playing audio
+        audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        // node for adjusting rate/pitch
+        let changeRatePitchNode = AVAudioUnitTimePitch()
+        if let pitch = pitch {
+            changeRatePitchNode.pitch = pitch
+        }
+        if let rate = rate {
+            changeRatePitchNode.rate = rate
+        }
+        audioEngine.attachNode(changeRatePitchNode)
+        
+        // node for echo
+        let echoNode = AVAudioUnitDistortion()
+        echoNode.loadFactoryPreset(.MultiEcho1)
+        audioEngine.attachNode(echoNode)
+        
+        // node for reverb
+        let reverbNode = AVAudioUnitReverb()
+        reverbNode.loadFactoryPreset(.Cathedral)
+        reverbNode.wetDryMix = 50
+        audioEngine.attachNode(reverbNode)
+        
+        // connect nodes
+        if echo == true && reverb == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, echoNode, reverbNode, audioEngine.outputNode)
+        } else if echo == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, echoNode, audioEngine.outputNode)
+        } else if reverb == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, reverbNode, audioEngine.outputNode)
+        } else {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, audioEngine.outputNode)
+        }
+        
+        // schedule to play and start the engine!
+        audioPlayerNode.stop()
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil) {
+            
+            var delayInSeconds: Double = 0
+            
+            if let lastRenderTime = self.audioPlayerNode.lastRenderTime, let playerTime = self.audioPlayerNode.playerTimeForNodeTime(lastRenderTime) {
+                
+                if let rate = rate {
+                    delayInSeconds = Double(self.audioFile.length - playerTime.sampleTime) / Double(self.audioFile.processingFormat.sampleRate) / Double(rate)
+                } else {
+                    delayInSeconds = Double(self.audioFile.length - playerTime.sampleTime) / Double(self.audioFile.processingFormat.sampleRate)
+                }
+            }
+            
+            // schedule a stop timer for when audio finishes playing
+            if self.stopButton.enabled == true {
+                self.stopTimer = NSTimer(timeInterval: delayInSeconds, target: self, selector: #selector(PlaySoundsViewController.stopAudio), userInfo: nil, repeats: false)
+                NSRunLoop.mainRunLoop().addTimer(self.stopTimer!, forMode: NSDefaultRunLoopMode)
+            }
+
+        }
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            showAlert(Alerts.AudioEngineError, message: String(error))
+            return
+        }
+        
+        // play the recording!
+        audioPlayerNode.play()
+    }
+    
+    
+    // MARK: Connect List of Audio Nodes
+    
+    func connectAudioNodes(nodes: AVAudioNode...) {
+        print("\(self.dynamicType).connectAudioNodes(\(nodes))")
+        for x in 0..<nodes.count-1 {
+            audioEngine.connect(nodes[x], to: nodes[x+1], format: audioFile.processingFormat)
+        }
+    }
+    
+    func stopAudio() {
+        print("\(self.dynamicType).stopAudio()")
+        
+        if let stopTimer = stopTimer {
+            stopTimer.invalidate()
+        }
+        
+        configureUI(.NotPlaying)
+        
+        if let audioPlayerNode = audioPlayerNode {
+            audioPlayerNode.stop()
+        }
+        
+        if let audioEngine = audioEngine {
+            audioEngine.stop()
+            audioEngine.reset()
+        }
+    }
+    
+    
+    // MARK: UI Functions
+
+    func configureUI(playState: PlayingState) {
+        print("\(self.dynamicType).configureUI(\(playState))")
+        switch(playState) {
+        case .Playing:
+            setPlayButtonsEnabled(false)
+            stopButton.enabled = true
+        case .NotPlaying:
+            setPlayButtonsEnabled(true)
+            stopButton.enabled = false
+        }
+    }
+    
+    func setPlayButtonsEnabled(enabled: Bool) {
+        print("\(self.dynamicType).setPlayButtonsEnabled(\(enabled))")
+        snailButton.enabled = enabled
+        chipmunkButton.enabled = enabled
+        rabbitButton.enabled = enabled
+        vaderButton.enabled = enabled
+        echoButton.enabled = enabled
+        reverbButton.enabled = enabled
+        playButton.enabled = enabled
+    }
+
+    
+    func showAlert(title: String, message: String) {
+        print("\(self.dynamicType).showAlert(\(title),\(message))")
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: Alerts.DismissAlert, style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
